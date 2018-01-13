423893568133046a2636cfbb603a56ef81edc3ce
Foad R ShariatPanahi <foadshariat@outlook.com>
2018-01-13 01:08:30 -0800

    Forms - Template driven

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,49 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+
+  @ViewChild('f') inputForm: NgForm;
+  defaultGender = "male";
+  color='';
+  animals=['dog', 'horse', 'cat', 'bee'];
+  user= {
+    username: '',
+    email: '',
+    gender: '',
+    color: '',
+    animal: ''
+  }
+  isSubmitted = false;
+
+  // onSubmit(form: NgForm){
+  //   console.log(form);
+  // }
+
+  onClick(){
+    // this.inputForm.setValue({
+    //   groupUser:{
+    //     username: 'ali',
+    //     email: ''
+    //   },
+    //   gender: '',
+    //   color: '',
+    //   animal: ''
+    // });
+    this.inputForm.form.patchValue({
+      groupUser: {
+        username: 'Foad'
+      }
+    });
+  }
+
+  onSubmit(){
+    this.isSubmitted = true;
+    this.user.username = this.inputForm.value.groupUser.username;
+    this.user.email = this.inputForm.value.groupUser.email;
+    this.user.gender = this.inputForm.value.gender;
+    this.user.color = this.inputForm.value.color;
+    this.user.animal = this.inputForm.value.animal;
+
+    this.inputForm.reset();
+  }
 }
